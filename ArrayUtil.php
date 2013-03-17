870aee13a2a5559795e3795e8ea8eae1cfcdb8be
Michael Fenwick <mike@mikefenwick.com>
2013-03-16 19:30:15 -0700

    Initial Commit

---
+++ ArrayUtil.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @author  Michael Fenwick <mike@mikefenwick.com>
+ * A PHP class full of helpful utility functions for filtering, sorting, and otherwise manipulating arrays in PHP.
+ */
+class ArrayUtil {
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,4 @@
+ArrayUtil
+============
+
+A PHP class full of helpful utility functions for filtering, sorting, and otherwise manipulating arrays in PHP.
